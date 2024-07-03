069a5e141eeb487c03d0a8f73644c7799cafbf1b
TigranBalayan10 <tigranbalayan10@gmail.com>
2024-07-03 01:44:31 -0700

    fixed error in provider slug type

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,16 @@ const healthCollection = defineCollection({
   }),
 });
 
+const healthProvidersCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+  }),
+});
+
 export const collections = {
   "health-insurance": healthCollection,
+  "health-providers": healthProvidersCollection,
 };
